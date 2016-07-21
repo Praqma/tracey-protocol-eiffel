e70358f969214544848bf20359327d95048cce1b
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-07-21 20:05:58 +0100

    Fix Issue.Transition type definition
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

---
+++ src/test/java/net/praqma/tracey/protocol/eiffel/utils/GitUtilsTest.java
@@ -0,0 +1,16 @@
+package net.praqma.tracey.protocol.eiffel.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andrey9kin on 21/07/16.
+ */
+public class GitUtilsTest {
+
+    @Test
+    public void testGetIssues() throws Exception {
+
+    }
+}
