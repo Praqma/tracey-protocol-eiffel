9cc24b348124f3e4e114b02b0f076c7c812d3aa8
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-07-21 16:43:45 +0100

    Added more BaseFactory constructors to simplify factory creation
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- src/main/java/net/praqma/tracey/protocol/eiffel/factories/BaseFactory.java
+++ src/main/java/net/praqma/tracey/protocol/eiffel/factories/BaseFactory.java
@@ -1,11 +1,14 @@
 package net.praqma.tracey.protocol.eiffel.factories;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
 import com.google.protobuf.Message;
+import com.jcabi.manifests.Manifests;
 import net.praqma.tracey.protocol.eiffel.models.Models.Link;
 import net.praqma.tracey.protocol.eiffel.models.Models.Data;
 import net.praqma.tracey.protocol.eiffel.models.Models.Meta;
@@ -17,6 +20,16 @@ public abstract class BaseFactory {
 
     public BaseFactory(final String host, final String name, final String uri, final String domainId, final Data.GAV gav) {
         source = Data.Source.newBuilder().setHost(host).setName(name).setUri(uri).setDomainId(domainId).setSerializer(gav).build();
+        log.fine("New factory with the source: " + source.toString());
+    }
+
+    public BaseFactory(final String name, final String uri, final String domainId, final Data.GAV gav) {
+        source = Data.Source.newBuilder().setHost(getHostName()).setName(name).setUri(uri).setDomainId(domainId).setSerializer(gav).build();
+        log.fine("New factory with the source: " + source.toString());
+    }
+
+    public BaseFactory(final String name, final String uri, final String domainId) {
+        source = Data.Source.newBuilder().setHost(getHostName()).setName(name).setUri(uri).setDomainId(domainId).setSerializer(getGAV()).build();
         log.fine("New factory with the source: " + source.toString());
     }
 
@@ -33,7 +46,30 @@ public abstract class BaseFactory {
         meta.setType(type);
         meta.setTime(java.lang.System.currentTimeMillis());
         meta.setSource(source);
-        log.fine("Create new meta " + meta.toString());
+        log.fine("Return meta: " + meta.toString());
         return meta.build();
+    }
+
+    private String getHostName() {
+        String hostname = "Unknown";
+        try
+        {
+            hostname = InetAddress.getLocalHost().getHostName();
+        }
+        catch (UnknownHostException e) {
+            log.warning("Hostname can not be resolved due to the following. Use " + hostname + " as a hostname\n" + e.getMessage());
+        }
+        log.fine("Return hostname: " + hostname);
+        return hostname;
+    }
+
+    private Data.GAV getGAV() {
+        Data.GAV gav = Data.GAV.newBuilder()
+                .setGroupId(Manifests.read("Implementation-Vendor-Id"))
+                .setArtifactId(Manifests.read("Implementation-Title"))
+                .setVersion(Manifests.read("Implementation-Version"))
+                .build();
+        log.fine("Return GAV: " + gav.toString());
+        return gav;
     }
 }
